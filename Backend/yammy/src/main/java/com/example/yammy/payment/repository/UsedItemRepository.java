35e5b22c459ec1409253bb7a8ba3ddf36bb07a39
김선우 <dora01622@naver.com>
2025-10-22 13:05:43 +0900

    feat: 마스터 브랜치 추가

--- Backend/yammy/src/main/java/com/example/yammy/payment/repository/UsedItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.yammy.payment.repository;
-import com.example.yammy.payment.entity.UsedItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// DB 조회 기능 제공
-public interface UsedItemRepository extends JpaRepository<UsedItem, Long> {
-}
