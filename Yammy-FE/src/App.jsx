6e6a9491c80a5ec7e683f373bd49f950b8cb9fc2
김선우 <dora01622@naver.com>
2025-10-27 15:33:43 +0900

    fix: swagger config info 수정

--- Yammy-FE/src/App.jsx
+++ Yammy-FE/src/App.jsx
@@ -1,11 +1,13 @@
 import NavigationBarBottom from './shared/components/NavagationBarBottom'
 import NavigationBarTop from './shared/components/NavigationBarTop'
+import UsedItem from './useditem/components/UsedItem'
 import "./App.css"
 
 function App() {
   return (
     <div className="app-container">
       <NavigationBarTop />
+      <UsedItem />
       <NavigationBarBottom />
     </div>
   );
