c5f5b016c4f1753ba3b2d31d8222b96174c3c87b
김태영 <erd57819@gmail.com>
2025-10-28 11:01:57 +0900

    fix:sns페이지 작성

--- Yammy-FE/src/App.jsx
+++ Yammy-FE/src/App.jsx
@@ -1,11 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import NavigationBarBottom from './shared/components/NavagationBarBottom'
 import NavigationBarTop from './shared/components/NavigationBarTop'
+import SNSPage from './sns/components/SNSPage'
+import UsedItem from './useditem/components/UsedItem'
 import "./App.css"
 
 function App() {
   return (
     <div className="app-container">
       <NavigationBarTop />
+      <Routes>
+        <Route path="/" element={<SNSPage />} />
+        <Route path="/useditem" element={<UsedItem />} />
+      </Routes>
       <NavigationBarBottom />
     </div>
   );
