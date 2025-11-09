a546a48a591ffe4453908294781355b87d86bae8
박재환 <pogpog1230@naver.com>
2025-11-09 17:09:39 +0900

    fix: front env, back api url

--- Yammy-BE/yammy/src/main/java/com/ssafy/yammy/config/SecurityConfig.java
+++ Yammy-BE/yammy/src/main/java/com/ssafy/yammy/config/SecurityConfig.java
@@ -110,9 +110,7 @@ public class SecurityConfig {
         // 개발 + 운영 도메인 함께 허용
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "http://k13c205.p.ssafy.io",
-                "http://k13c205.p.ssafy.io:3000",
-                "http://k13c205.p.ssafy.io:8080"
+                "http://k13c205.p.ssafy.io"
         ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
